1f33ea5767ff2cc6d748a51d20e55f16adfffed8
ghouili <walid.ghouili98@gmail.com>
2023-04-23 21:06:13 +0100

    23/04

---
+++ Routes/user.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const UserController = require('../Controllers/user');
+const user = require('../Models/user');
+
+const route = express.Router();
+
+route.get('/', UserController.GetAll);
+
+route.get('/:id', UserController.FindById);
+
+route.put('/:id', UserController.Update);
+
+route.delete('/:id', UserController.Delete);
+
+route.post('/register', UserController.Register);
+
+route.post('/login', UserController.Login);
+
+module.exports = route
